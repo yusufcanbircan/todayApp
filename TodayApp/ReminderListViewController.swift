92e13bc3f358f69ba0566357999c9211df372609
yusufcanbircan <yusufcannnz@hotmail.com>
2022-09-08 15:59:09 +0300

    diffable data source has created

---
+++ TodayApp/ReminderListViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  TodayApp
+//
+//  Created by Yusuf Can Bircan on 8.09.2022.
+//
+
+import UIKit
+
+class ReminderListViewController: UICollectionViewController {
+    
+    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, String>
+    
+    var dataSource: DataSource!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+            
+        let listLayout = listLayout()
+        collectionView.collectionViewLayout = listLayout
+        
+        let cellRegistration = UICollectionView.CellRegistration { (cell:UICollectionViewListCell, indexPath: IndexPath, itemIndetifier: String) in
+            
+            let reminder = Reminder.sampleData[indexPath.item]
+            var contentConfiguration = cell.defaultContentConfiguration()
+            contentConfiguration.text = reminder.title
+            cell.contentConfiguration = contentConfiguration
+        }
+        
+        dataSource = DataSource(collectionView: collectionView) { (collectionView: UICollectionView, indexPath: IndexPath, itemIdentifier: String) in
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
+        }
+        
+        var snapshot = Snapshot()
+        snapshot.appendSections([0])
+        snapshot.appendItems(Reminder.sampleData.map{ $0.title })
+        
+        dataSource.apply(snapshot)
+        collectionView.dataSource = dataSource
+    }
+    
+    private func listLayout() -> UICollectionViewCompositionalLayout {
+        
+        var listConfiguration = UICollectionLayoutListConfiguration(appearance: .grouped)
+        listConfiguration.showsSeparators = false
+        listConfiguration.backgroundColor = .clear
+        
+        return UICollectionViewCompositionalLayout.list(using: listConfiguration)
+    }
+
+
+}
+
